70ea63b3d4e31582eb5e2758ec635d61d3adae43
Gaurab Banerjee <gaurab.banerjee97@gmail.com>
2021-11-27 19:43:49 -0700

    learning attempt

--- main.py
+++ main.py
@@ -2,15 +2,29 @@ from CircleOfDeathEnv import CircleOfDeath
 import numpy as np
 import gym
 from gym import spaces
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common import make_vec_env
+from stable_baselines import A2C
 
 env = CircleOfDeath()
+model = A2C('MlpPolicy', env, verbose=1).learn(total_timesteps=100)
 
-env.reset()
-for _ in range(10):
+obs = env.reset()
+
+dones = False
+while not dones:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
     env.render()
-    # print(env.action_space.sample())
-    state, reward, done, _ = env.step(env.action_space.sample()) # take a random action
-    print(reward)
-env.close()
-# Define and Train the agent
-# model = A2C('CnnPolicy', env).learn(total_timesteps=1000)
+
+# for episode in range(1):
+#     observation = env.reset()
+#     for t in range(10):
+#         env.render()
+#         print(observation)
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         if done:
+#             print("Episode finished after {} timesteps".format(t+1))
+#             break
+# env.close()
