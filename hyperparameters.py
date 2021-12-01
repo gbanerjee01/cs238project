b23f40eeb8bb72ebf612799da838b5a2e2d9b093
Gaurab Banerjee <gaurab.banerjee97@gmail.com>
2021-12-01 09:13:31 -0700

    logging code

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,4 +1,13 @@
+algorithm = "qlearning" #qlearning OR sarsa
+n_adversaries = 0
+
+
 false_positive_rate = 0.05
 false_negative_rate = 0.15
-n_adversaries = 3
-n_episodes = 101
+n_episodes = 1000001
+test_eps = 100001
+
+exp_dir = "./experiments/" + algorithm + "/"
+exp_name = str(n_adversaries) + "advs" + str(false_positive_rate) + "fpr" + str(false_negative_rate) + "fnr" + "_"
+
+exp_file_prefix = exp_dir + exp_name
--- qlearning.py
+++ qlearning.py
@@ -22,6 +22,10 @@ epsilon = 0.5
 all_epochs = []
 all_penalties = []
 
+episode_rewards = []
+episode_iterations = []
+episode_result = []
+
 for i in tqdm(range(1, hyperparameters.n_episodes)):
     new_env = env.reset()
     state = new_env['cur_loc']
@@ -41,9 +45,15 @@ for i in tqdm(range(1, hyperparameters.n_episodes)):
     # env.render()
     epochs, penalties, reward, = 0, 0, 0
     done = False
+
+    n_iterations = 0
+    episode_reward = 0
+    episode_successful = False
     
     #q code referenced from a towardsdatascience article and then adapted
     while not done:
+        n_iterations += 1
+
         if random.uniform(0, 1) < epsilon:
             action = env.action_space.sample() # Explore action space
         else:
@@ -51,6 +61,8 @@ for i in tqdm(range(1, hyperparameters.n_episodes)):
 
         next_state, reward, done, info = env.step(action) 
         # env.render()
+
+        episode_reward += reward
         
         old_value = q_table[state, goal_num, action]
         next_max = np.max(q_table[next_state['cur_loc'], goal_num])
@@ -63,17 +75,35 @@ for i in tqdm(range(1, hyperparameters.n_episodes)):
 
         state = next_state['cur_loc']
         epochs += 1
+
+        if done and env.state["cur_loc"] in env.state["exit_goal"]:
+            episode_successful = True
+
+    episode_iterations.append(n_iterations)
+    episode_rewards.append(episode_reward)
+    episode_result.append(episode_successful)
         
     # if i % 1000 == 0:
     #     # clear_output(wait=True)
     #     print(f"Episode: {i}")
 
 print("Training finished.\n")
+# breakpoint()
+
+#TODO Save q_table and three lists
+np.save(exp_file_prefix + "q_table", q_table)
+np.save(exp_file_prefix + "episode_rewards", episode_rewards)
+np.save(exp_file_prefix + "episode_iterations", episode_iterations)
+np.save(exp_file_prefix + "episode_result", episode_result)
+
+test_actions_len = []
+test_result = []
+test_rewards = []
 
 actions_list = []
-for episode in range(1):
+for episode in tqdm(range(hyperparameters.test_eps)):
     observation = env.reset()
-    env.render()
+    # env.render()
     goal_list = observation['exit_goal']
     if goal_list == env.exit_zoneNorth: 
         goal_num = 0
@@ -84,14 +114,33 @@ for episode in range(1):
     else: 
         goal_num = 3
 
-    for t in range(10):
+    ep_reward = 0
+    ep_success = False
+
+    while True:
         # print(observation)
         action = np.argmax(q_table[observation['cur_loc'], goal_num])
         actions_list.append(action)
         observation, reward, done, info = env.step(action)
-        env.render()
+
+        ep_reward += reward
+        # env.render()
         if done:
-            print("Episode finished after {} timesteps".format(t+1))
+            if env.state["cur_loc"] in env.state["exit_goal"]:
+                ep_success = True
+
+
+            test_actions_len.append(len(actions_list))
+            test_result.append(ep_success)
+            test_rewards.append(ep_reward)
+            # print("Episode finished after {} timesteps".format(t+1))
             break
 # breakpoint()
+
+np.save(exp_file_prefix + "test_actions_len", test_actions_len)
+np.save(exp_file_prefix + "test_result", test_result)
+np.save(exp_file_prefix + "test_rewards", test_rewards)
+
+print("Testing finished")
+#TODO: SAVE LISTS w/ exp name
 env.close()
